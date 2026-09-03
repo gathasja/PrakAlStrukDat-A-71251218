bd42b7ea32115ed33c91593fa472e63cbc07fa44
gatha <gathasja@gmail.com>
2026-09-03 14:17:47 +0700

    week1

---
+++ piramida angka.py
@@ -0,0 +1,15 @@
+def piramida_angka(angka):
+    for i in range(1, angka + 1):
+        print(' ' * (angka - i) * 2, end='')
+        for j in range(1, i+1):
+            if j == i:
+                print(j, end='')
+            else:
+                print(j, end=' ')
+        for j in range(i - 1, 0, -1):
+            if j == 1:
+                print(" " + str(j), end="")
+            else:
+                print(" " + str(j), end="")
+        print()  
+piramida_angka(9)
