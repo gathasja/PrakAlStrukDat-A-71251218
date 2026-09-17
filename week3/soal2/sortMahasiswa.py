29777b403e6407d0a46bd9a4d9d07b13f34a41c8
gatha <gathasja@gmail.com>
2026-09-17 14:39:46 +0700

    71251218

--- week3/soal2/sortMahasiswa.py
+++ week3/soal2/sortMahasiswa.py
@@ -16,6 +16,23 @@ def sort_by(data: list=data, index: str="nim",rev = False):
     }
     
     # Kerjakan disini
+    col = maps[index]
+    n = len(data)
+
+    for i in range(n - 1):
+        swapped = False
+        for j in range(n - 1 - i):
+            nilai_kiri = data[j][col]
+            nilai_kanan = data[j + 1][col]
+
+            # ascending (rev=False) -> tukar jika kiri > kanan
+            # descending (rev=True) -> tukar jika kiri < kanan
+            if (not rev and nilai_kiri > nilai_kanan) or (rev and nilai_kiri < nilai_kanan):
+                data[j], data[j + 1] = data[j + 1], data[j]
+                swapped = True
+
+        if not swapped:
+            break
     
     # Jangan Dihapus
     show_data(data)
